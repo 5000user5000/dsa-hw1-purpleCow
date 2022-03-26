00b1d7594f60ed3ee3546fb26086e3c7b1f7c433
5000user5000 <37620231+5000user5000@users.noreply.github.com>
2022-03-26 11:07:22 +0800

    Update strtok.c

--- func/strtok.c
+++ func/strtok.c
@@ -3,7 +3,7 @@
 #include <string.h> // strtok
 int convert(char num[]);
 int main() {
-    char str[] = "enter 123456789 300 425";
+    char str[] = "enter 123456789 300 425";//如果要輸入,用fget(因為scanf讀到空格就停了)
     const char* d = "  ";
     char *p;
     int order[15],i=0;//order 最多有3*10^6個指令->malloc
@@ -14,7 +14,7 @@ int main() {
     
     while (p != NULL) {
         //printf("%s\n", p);
-        order[i++]=convert(p);//存數字
+        order[i++]=convert(p);//存數字,其實可以用內建的atoi函數就好
         p = strtok(NULL, d);		   
     }
     for(int k=0;k<3;k++)printf("%d\n",order[k] );
